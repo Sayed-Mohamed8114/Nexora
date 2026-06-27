8786d01b44a709c6a185073deadfe550a00efe4f
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-06-27 22:48:58 +0300

    main setup with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+<>
+<h1 className="text-black font-bold text-8xl bg-linear-to-r from-slate-900 via-slate-500 to-slate-100 m-2 p-2">
+  react and tailwind is done
+</h1>
+</>
+  )
+}
+
+export default App
