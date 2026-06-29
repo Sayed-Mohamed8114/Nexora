c2449c799f455629d4778c73b5dd42df8132e594
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-06-29 14:22:05 +0300

    set up routes and add home route to make sure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
+import AppRouter from "./Routes/AppRouter";
 
 function App() {
-
-  return (
-<>
-<h1 className="text-black font-bold text-8xl bg-linear-to-r from-slate-900 via-slate-500 to-slate-100 m-2 p-2">
-  react and tailwind is done
-</h1>
-</>
-  )
+  return <AppRouter/>
 }
 
-export default App
+export default App;
