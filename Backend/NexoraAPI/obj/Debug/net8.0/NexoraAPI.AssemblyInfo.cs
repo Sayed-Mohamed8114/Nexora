4ba9e41d07119c8b699ebd22f139532e3afac4bc
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-07-16 02:36:47 +0300

    rename it

--- Backend/NexoraAPI/obj/Debug/net8.0/NexoraAPI.AssemblyInfo.cs
+++ Backend/NexoraAPI/obj/Debug/net8.0/NexoraAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b93f6a8d877136579693b48921768788accdf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744b0c2d02ccac5de7bd989eefaf55f3bf2cd2ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
